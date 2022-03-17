3c857b4fe3579f649a33ef5b7054c8064f516ad0
Pirozh-ok <70931484+Pirozh-ok@users.noreply.github.com>
2022-03-17 13:49:26 +0700

    Слайдер готов

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-//Инициализируем Swiper
-new Swiper('.image-slider',{
-    //стрелки
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
     },
-})
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
